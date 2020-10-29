dca939ee0c09ba8caf6c33b094936695068cd81f
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-10-29 20:53:11 +0100

    Highlights: styled Highlights component

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -7,7 +7,16 @@ export default function Highlights() {
     {
       heroImage: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603923446/yoni-images/postimage1_exlyui.jpg",
       title: "10 Fatos surpreendentes sobre orgasmo que você realmente quer saber",
-      slug: "10-fatos-surpreendentes-sobre-orgasmo-que-voce-realmente-quer-saber",
+      slug: "10-fatos-surpreendentes-sobre-orgasmo-que-voce-realmente-quer-saber-1",
+      category: "Sexualidade",
+      authorImg: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603912363/yoni-images/avatar1.png",
+      author: "Sarah Jio",
+      publishDate: "01, outubro 2019",
+    },
+    {
+      heroImage: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603923446/yoni-images/postimage1_exlyui.jpg",
+      title: "Ser feminista e se relacionar com homens — O dilema.",
+      slug: "ser-feminista-e-se-relacionar-com-homens-o-dilema",
       category: "Sexualidade",
       authorImg: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603912363/yoni-images/avatar1.png",
       author: "Sarah Jio",
@@ -16,7 +25,7 @@ export default function Highlights() {
     {
       heroImage: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603923446/yoni-images/postimage1_exlyui.jpg",
       title: "10 Fatos surpreendentes sobre orgasmo que você realmente quer saber",
-      slug: "10-fatos-surpreendentes-sobre-orgasmo-que-voce-realmente-quer-saber",
+      slug: "10-fatos-surpreendentes-sobre-orgasmo-que-voce-realmente-quer-saber-3",
       category: "Sexualidade",
       authorImg: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603912363/yoni-images/avatar1.png",
       author: "Sarah Jio",
@@ -25,7 +34,79 @@ export default function Highlights() {
     {
       heroImage: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603923446/yoni-images/postimage1_exlyui.jpg",
       title: "10 Fatos surpreendentes sobre orgasmo que você realmente quer saber",
-      slug: "10-fatos-surpreendentes-sobre-orgasmo-que-voce-realmente-quer-saber",
+      slug: "10-fatos-surpreendentes-sobre-orgasmo-que-voce-realmente-quer-saber-4",
+      category: "Sexualidade",
+      authorImg: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603912363/yoni-images/avatar1.png",
+      author: "Sarah Jio",
+      publishDate: "01, outubro 2019",
+    },
+    {
+      heroImage: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603923446/yoni-images/postimage1_exlyui.jpg",
+      title: "Ser feminista e se relacionar com homens — O dilema.",
+      slug: "ser-feminista-e-se-relacionar-com-homens-o-dilema-2",
+      category: "Sexualidade",
+      authorImg: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603912363/yoni-images/avatar1.png",
+      author: "Sarah Jio",
+      publishDate: "01, outubro 2019",
+    },
+    {
+      heroImage: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603923446/yoni-images/postimage1_exlyui.jpg",
+      title: "10 Fatos surpreendentes sobre orgasmo que você realmente quer saber",
+      slug: "10-fatos-surpreendentes-sobre-orgasmo-que-voce-realmente-quer-saber-5",
+      category: "Sexualidade",
+      authorImg: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603912363/yoni-images/avatar1.png",
+      author: "Sarah Jio",
+      publishDate: "01, outubro 2019",
+    },
+    {
+      heroImage: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603923446/yoni-images/postimage1_exlyui.jpg",
+      title: "10 Fatos surpreendentes sobre orgasmo que você realmente quer saber",
+      slug: "10-fatos-surpreendentes-sobre-orgasmo-que-voce-realmente-quer-saber-6",
+      category: "Sexualidade",
+      authorImg: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603912363/yoni-images/avatar1.png",
+      author: "Sarah Jio",
+      publishDate: "01, outubro 2019",
+    },
+    {
+      heroImage: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603923446/yoni-images/postimage1_exlyui.jpg",
+      title: "Ser feminista e se relacionar com homens — O dilema.",
+      slug: "ser-feminista-e-se-relacionar-com-homens-o-dilema4",
+      category: "Sexualidade",
+      authorImg: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603912363/yoni-images/avatar1.png",
+      author: "Sarah Jio",
+      publishDate: "01, outubro 2019",
+    },
+    {
+      heroImage: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603923446/yoni-images/postimage1_exlyui.jpg",
+      title: "10 Fatos surpreendentes sobre orgasmo que você realmente quer saber",
+      slug: "10-fatos-surpreendentes-sobre-orgasmo-que-voce-realmente-quer-saber-7",
+      category: "Sexualidade",
+      authorImg: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603912363/yoni-images/avatar1.png",
+      author: "Sarah Jio",
+      publishDate: "01, outubro 2019",
+    },
+    {
+      heroImage: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603923446/yoni-images/postimage1_exlyui.jpg",
+      title: "10 Fatos surpreendentes sobre orgasmo que você realmente quer saber",
+      slug: "10-fatos-surpreendentes-sobre-orgasmo-que-voce-realmente-quer-saber-8",
+      category: "Sexualidade",
+      authorImg: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603912363/yoni-images/avatar1.png",
+      author: "Sarah Jio",
+      publishDate: "01, outubro 2019",
+    },
+    {
+      heroImage: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603923446/yoni-images/postimage1_exlyui.jpg",
+      title: "Ser feminista e se relacionar com homens — O dilema.",
+      slug: "ser-feminista-e-se-relacionar-com-homens-o-dilema-7",
+      category: "Sexualidade",
+      authorImg: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603912363/yoni-images/avatar1.png",
+      author: "Sarah Jio",
+      publishDate: "01, outubro 2019",
+    },
+    {
+      heroImage: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603923446/yoni-images/postimage1_exlyui.jpg",
+      title: "10 Fatos surpreendentes sobre orgasmo que você realmente quer saber",
+      slug: "10-fatos-surpreendentes-sobre-orgasmo-que-voce-realmente-quer-saber-9",
       category: "Sexualidade",
       authorImg: "https://res.cloudinary.com/dcnqq10ni/image/upload/v1603912363/yoni-images/avatar1.png",
       author: "Sarah Jio",
@@ -41,18 +122,23 @@ export default function Highlights() {
       <div className="flexRowContainer">
         {
           highlightsPosts.map(post => (
-            <Link href={`/post/${post.slug}`}>
-              <a className="postContainer">
-                <div>
+            <Link href={`/post/${post.slug}`} key={post.slug}>
+              <a>
+                <div className="postImageContainer">
                   <img src={post.heroImage} alt={post.title}/>
                 </div>
-                <p>{post.category}</p>
-                <h1>{post.title}</h1>
-                <div>
-                  <img src={post.authorImg} alt={post.author}/>
-                  <p>{post.author}</p>
-                  <span>♥</span>
-                  <span>{post.publishDate}</span>
+                <div className="postInfoContainer">
+                  <p>{post.category}</p>
+                  <div>
+                    <h2>{post.title}</h2>
+                    <div className="postAuthorContainer">
+                      <img src={post.authorImg} alt={post.author}/>
+                      <div>
+                        <p>{post.author} <span>♥</span></p>
+                        <span>{post.publishDate}</span>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </a>
             </Link>
--- src/styles/components/Highlights.ts
+++ src/styles/components/Highlights.ts
@@ -1,15 +1,108 @@
 import styled from 'styled-components';
+import { marineBlueColor, mustardColor, blueBerryColor } from '../colors';
 
 export const HighlightsContainer = styled.section`
-  width: 100vw;
+  width: 100%;
+  padding: 100px 0;
+
+  h1 {
+    font-size: 1.25rem;
+    padding-left: 20px;
+    margin-bottom: 20px;
+  }
 
   .flexRowContainer {
     display: flex;
     overflow: scroll;
+    width: 100%;
+
+    /* &::-webkit-scrollbar {
+      display: none;
+    } */
 
     a {
-      height: 310px;
       width: 155px;
+      height: 100%;
+      border: 1px solid rgba(0,0,0,0.2);
+
+      &:first-of-type {
+        margin-left: 20px;
+      }
+
+      a:last-of-type {
+        margin-right: 20px;
+      }
     }
+
+    a + a {
+      margin-left: 10px;
+    }
+
+    .postImageContainer {
+      height: 155px;
+      width: 155px;
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit:cover;
+      }
+    }
+
+    .postInfoContainer {
+      display: flex;
+      flex-direction: column;
+      height: 155px;
+      padding: 5px 10px;
+
+      > div {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+      }
+
+      > p {
+        color: ${mustardColor};
+        text-align: center;
+      }
+
+      h2 {
+        font-size: 0.9rem;
+        line-height: 1rem;
+        color: ${marineBlueColor};
+        margin: 5px 0;
+      }
+
+      .postAuthorContainer {
+        align-self: baseline;
+        display: flex;
+        align-items: center;
+        padding: 0 5px;
+        margin-bottom: 5px; 
+
+        img {
+          width: 32px;
+          height: 32px;
+          object-fit:cover;
+          border-radius: 50%;
+          margin-right: 10px;
+        }
+
+        p {
+          color: ${blueBerryColor};
+          font-size: 0.75rem;
+        }
+        
+        span {
+          color: ${blueBerryColor};
+          font-size: 0.6rem;
+        }
+      }
+    }
+  }
+
+  @media (min-width: 899px) {
+    padding: 10vh 0 7vh;
   }
 `;
