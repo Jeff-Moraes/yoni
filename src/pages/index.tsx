423cae790667f361f4e4b7a55f9c15e7dc9be85a
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-10-30 00:16:10 +0100

    Footer: created Footer component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Header from '../components/Header';
 import About from '../components/About';
 import Categories from '../components/Categories';
 import Highlights from '../components/Highlights';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Categories />
       <Highlights />
+      <Footer />
     </div>
   )
 }
