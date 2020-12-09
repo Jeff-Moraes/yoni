df36225777a7f1f55b470061f6f073a34729955b
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-12-09 15:52:02 +0100

    Added RDSM script

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -36,10 +36,14 @@ export default class MyDocument extends Document {
 
           {/* <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/> */}
           <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600&display=swap" rel="stylesheet" />
+
         </Head>
         <body>
           <Main />
           <NextScript />
+          
+          {/* Formulário integrado RDSM */}
+          <script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/9c2b69bb-fa44-4d5a-9e64-1c28f595697f-loader.js" ></script>
         </body>
       </Html>
     )
